439ca549c294cdd4b92aa6f1b5946ce85d080a74
Manji Hirani <hiranidotcom@gmail.com>
2026-05-29 11:23:10 -0500

    Finished Netflix website cloning project

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,58 @@
+import React, { useEffect } from "react";
+import "./Navbar.css";
+import logo from "../../assets/logo.png";
+import search_icon from "../../assets/search_icon.svg";
+import bell_icon from "../../assets/bell_icon.svg";
+import profile_img from "../../assets/profile_img.png";
+import caret_icon from "../../assets/caret_icon.svg";
+import { logout } from "../../firebase";
+
+const Navbar = () => {
+    const navRef = React.useRef();
+
+    useEffect(() => {
+        window.addEventListener("scroll", () => {
+            if (window.scrollY >= 80) {
+                navRef.current.classList.add("nav_dark");
+            } else {
+                navRef.current.classList.remove("nav_dark");
+            }
+        });
+    }, []);
+
+    return (
+        <div ref={navRef} className="navbar">
+            <div className="navbar__left">
+                <img src={logo} alt="" className="navbar__logo" />
+                <ul className="navbar__links">
+                    <li className="navbar__link">Home</li>
+                    <li className="navbar__link">TV Shows</li>
+                    <li className="navbar__link">Movies</li>
+                    <li className="navbar__link">New & Popular</li>
+                    <li className="navbar__link">My List</li>
+                    <li className="navbar__link">Browse by Languages</li>
+                </ul>
+            </div>
+            <div className="navbar__right">
+                <img src={search_icon} alt="" className="icons" />
+                <p> Children</p>
+                <img src={bell_icon} alt="" className="icons" />
+                <div className="navbar__profile">
+                    <img src={profile_img} alt="" className="profile" />
+                    <img src={caret_icon} alt="" />
+                    <div className="dropdown">
+                        <p
+                            onClick={() => {
+                                logout()
+                            }}
+                        >
+                            Sign Out of Netflix
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
